279f9542dd64ca1b92f208301261c275c59aa667
qsparks <q.m.sparks@gmail.com>
2020-10-27 18:58:45 -0500

    adds persistent login

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,13 +1,26 @@
-
 import { useEffect } from 'react';
 import {NavLink} from 'react-router-dom';
 
+import UserModel from '../../models/UserModel';
+
+import {useRecoilState} from 'recoil';
+import {userState} from '../../recoil/atoms';
+
 const Sidebar = props => {
-    
+    const [user, setUser] = useRecoilState(userState);
 
     useEffect(function() {
+        if (localStorage.getItem('uid')) {
+            UserModel.show().then(response => {
+                setUser(response.data);
+            })
+        }
+    }, [setUser]);
 
-    })
+    const logout = () => {
+        setUser(null);
+        localStorage.clear();
+    }
 
     return (
         <aside>
@@ -19,25 +32,41 @@ const Sidebar = props => {
                         </NavLink>
                     </li>
                     <li>
-                        <NavLink to={'/register'}>
-                            Register
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to={'/login'}>
-                            Login
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to={'/dashboard'}>
-                            Dashboard
-                        </NavLink>
-                    </li>
-                    <li>
                         <NavLink to={'/tools'}>
                             Tool Generator
                         </NavLink>
                     </li>
+                    {
+                        user ? (
+                            <>
+                            <li>
+                            <NavLink to={'/dashboard'}>
+                                Dashboard
+                            </NavLink>
+                            </li>
+                            <li className="btn" onClick={logout}>
+                                Log Out
+                            </li>
+                            </>
+                        ) : (
+                            <>
+                            <li>
+                                <NavLink to={'/register'}>
+                                    Register
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to={'/login'}>
+                                    Login
+                                </NavLink>
+                            </li>
+                            </>
+                        )
+                    }
+                    
+                    
+                    
+                    
                 </ul>
             </div>
         </aside>
